cc0505973f87eb5dfd49871f15e659ec3f92e91c
Pankorn <sepankorn@gmail.com>
2019-02-21 20:51:47 +0700

    Structure Add

---
+++ Exercise4_Pankorn_W.py
@@ -0,0 +1,8 @@
+english_foundation = input('Enter you\'re English score')
+computer_science = input('Enter you\'re Computer score')
+eng_var = english_foundation
+computer_var = computer_science
+print('---You\'re score')
+print('Foundation English:',eng_var)
+print('Introduction to computer systems',computer_var)
+
