7760e42ac356a614c1d742cbaf499178b4fb33f7
Pankorn <sepankorn@gmail.com>
2019-02-21 21:09:09 +0700

    Complete Program
    Big change.

--- Exercise4_Pankorn_W.py
+++ Exercise4_Pankorn_W.py
@@ -1,8 +1,19 @@
-english_foundation = input('Enter you\'re English score')
-computer_science = input('Enter you\'re Computer score')
-eng_var = english_foundation
-computer_var = computer_science
-print('---You\'re score')
+# Score data for University student
+
+foundation_english = input('Enter you\'re Foundation English score:')
+introduction_computer = input('Enter you\'re Introduction To Computer Systems score:')
+general_business = input('Enter you\'re General Business score:')
+computer_programming = input('Enter you\'re Computer Programming score:')
+
+eng_var = foundation_english
+computer_var = introduction_computer
+business_var = general_business
+programming_var = computer_programming
+
+
+print('---You\'re score---')
 print('Foundation English:',eng_var)
-print('Introduction to computer systems',computer_var)
+print('Introduction To Computer Systems:',computer_var)
+print('General Buiness:',business_var)
+print('Computer Programming:',programming_var)
 
